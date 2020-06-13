83173d0dad790d977934e606855ed4b00aa63bc4
GGGasGit <gasparads@gmail.com>
2020-06-13 16:26:49 +0200

    new features added

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
+let totalAmount
+let orderedAmount
+let price
+let selectedPrice
+let totalPayable
+let priceTag
+
 function sendMessage() {
+    orderedAmount = document.querySelector("input[name='orderAmount']");
+    totalAmount = parseInt(orderedAmount.value);
+    if (totalAmount < 0 || totalAmount == 0 || isNaN(totalAmount)) {
+        alert("Valami darabot azért adjá' má' meg!");
+    } else {
     alert("Köszike a megrendelést, küldjük az árut!");
+}
 }
 
 function calculateAmount() {
-    let orderedAmount = document.querySelector("input[name='orderAmount']");
-    let totalAmount = parseInt(orderedAmount.value);
-    let price = document.querySelector("select[name='kebabType']");
-    let selectedPrice = parseInt(price.value);
-    let totalPayable = selectedPrice * totalAmount;
-    let priceTag = document.querySelector("span.totalPrice")
+    orderedAmount = document.querySelector("input[name='orderAmount']");
+    totalAmount = parseInt(orderedAmount.value);
+    totalAmount = isNaN(totalAmount) ? 0 : totalAmount;
+    if (totalAmount < 0) {
+        totalAmount = 0;
+    }
+    price = document.querySelector("select[name='kebabType']");
+    selectedPrice = parseInt(price.value);
+    totalPayable = selectedPrice * totalAmount;
+    priceTag = document.querySelector("span.totalPrice");
     priceTag.innerHTML = totalPayable;
 }
